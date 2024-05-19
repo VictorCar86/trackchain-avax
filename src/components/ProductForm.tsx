f32eff42b430392839c4612b7a595c2b39eac437
Victor Carreno <vmanuelcar@hotmail.com>
2024-05-19 14:54:54 -0500

    Update provider form logic

--- src/components/ProductForm.tsx
+++ src/components/ProductForm.tsx
@@ -1,16 +1,11 @@
 /* Components */
+import React, { useState } from "react";
 import { useContext } from "react";
 import { TrackContext } from "../context/context";
+import { useWeb3Modal } from '@web3modal/wagmi/react';
 
 function ProductForm() {
-    const inputStyles = "w-72 py-2 px-3 border border-gray-500 rounded-[12px] font-medium";
     const context = useContext(TrackContext);
-  
-import React, { useState } from "react";
-import { useWeb3Modal } from '@web3modal/wagmi/react'
-import { abi } from "../utils/constants";
-
-function ProductForm() {
     const inputStyles = "w-72 py-2 px-3 border border-gray-500 rounded-[12px] font-medium";
     const [priceOrder, setOrderPrice] = useState(0);
     const [statusOrder, setOrderStatus] = useState(0);
@@ -71,147 +66,89 @@ function ProductForm() {
 
                 <hr className="shadow-[0px_0px_1px_0px_#cdcdcd]" />
                 
-                {
-                    context.navDashBtn1 ? (
-                        <form className="mt-6 mb-6">
-                            <div className="grid grid-cols-2 grid-rows-4 gap-x-4 gap-y-3 w-max mx-auto">
-                                <label>
-                                    <p className="mb-1">
-                                        Product Name:
-                                    </p>
-                                    <input type="text" name="name" placeholder="Mesa PC" className={inputStyles} />
-                                </label>
-                                <label>
-                                    <p className="mb-1">
-                                        Product Price:
-                                    </p>
-                                    <input type="number" name="price" placeholder="$100" className={inputStyles} />
-                                </label>
-                                <label>
-                                    <p className="mb-1">
-                                        Create Date:
-                                    </p>
-                                    <input type="date" name="date" className={inputStyles} />
-                                </label>
-                                <label>
-                                    <p className="mb-1">
-                                        Delivery Date:
-                                    </p>
-                                    <input type="date" name="delivery" className={inputStyles} />
-                                </label>
-                                <label>
-                                    <p className="mb-1">
-                                        Product Code:
-                                    </p>
-                                    <input type="text" name="code" placeholder="123ed" className={inputStyles} />
-                                </label>
-                                <label>
-                                    <p className="mb-1">
-                                        Phone Number:
-                                    </p>
-                                    <input type="tel" name="phone" placeholder="+57 321 123 456" className={inputStyles} />
-                                </label>
-                                <label>
-                                    <p className="mb-1">
-                                        Delivery Status:
-                                    </p>
-                                    <input type="text" name="1" placeholder="Current state" className={inputStyles} />
-                                </label>
-                                <label>
-                                    <p className="mb-1">
-                                        Wallet User:
-                                    </p>
-                                    <input type="text" name="status" placeholder="0x4358c4455175b370E04b35D2c132993980294C62" className={inputStyles} />
-                                </label>
-                            </div>
+                {context.navDashBtn1 ? (
+                    <form className="mt-6 mb-6" onSubmit={sendPayload}>
+                        <div className="grid grid-cols-2 grid-rows-4 gap-x-4 gap-y-3 w-max mx-auto">
+                            <label>
+                                <p className="mb-1">
+                                    Product Name:
+                                </p>
+                                <input
+                                    type="text" name="name"
+                                    placeholder="No product"
+                                    className={inputStyles}
+                                    onInput={(e: React.ChangeEvent<HTMLInputElement>) => formDataUpdate("product", e.target.value)}
+                                />
+                            </label>
+                            <label>
+                                <p className="mb-1">
+                                    Product Price:
+                                </p>
+                                <input
+                                    type="number" name="price"
+                                    placeholder="$ - - -" className={inputStyles}
+                                    onInput={(e: React.ChangeEvent<HTMLInputElement>) => setOrderPrice(parseInt(e.target.value))}
+                                />
+                            </label>
+                            <label>
+                                <p className="mb-1">
+                                    Create Date:
+                                </p>
+                                <input
+                                    type="date" name="date" className={inputStyles}
+                                    onInput={(e: React.ChangeEvent<HTMLInputElement>) => formDataUpdate("date", e.target.value)}
+                                />
+                            </label>
+                            <label>
+                                <p className="mb-1">
+                                    Delivery Date:
+                                </p>
+                                <input
+                                    type="date" name="delivery" className={inputStyles}
+                                    onInput={(e: React.ChangeEvent<HTMLInputElement>) => formDataUpdate("delivery_date", e.target.value)}
+                                />
+                            </label>
+                            <label>
+                                <p className="mb-1">
+                                    Product Code:
+                                </p>
+                                <input
+                                    type="text" name="code" placeholder="TC-XXXX" className={inputStyles}
+                                    onInput={(e: React.ChangeEvent<HTMLInputElement>) => formDataUpdate("code", e.target.value)}
+                                />
+                            </label>
+                            <label>
+                                <p className="mb-1">
+                                    Phone Number:
+                                </p>
+                                <input
+                                    type="tel" name="phone"
+                                    placeholder="+57 XXX XXX XXXX" className={inputStyles}
+                                    onInput={(e: React.ChangeEvent<HTMLInputElement>) => formDataUpdate("cel_number", e.target.value)}
+                                />
+                            </label>
+                            <label>
+                                <p className="mb-1">
+                                    Delivery Status:
+                                </p>
+                                <input
+                                    type="text" name="status"
+                                    placeholder="Current state" className={inputStyles}
+                                    onInput={(e: React.ChangeEvent<HTMLInputElement>) => setOrderStatus(parseInt(e.target.value))}
+                                />
+                            </label>
+                            <label>
+                                <p className="mb-1">
+                                    Wallet User:
+                                </p>
+                                <input type="text" name="status" placeholder="Number wallet" className={inputStyles} />
+                            </label>
+                        </div>
 
+                        <button className="block my-5 mx-auto hover:border-black hover:border border-black border" type="submit">Submit</button>
 
-                            <button className="block my-5 mx-auto hover:border-black hover:border border-black border" type="button">Submit</button>
-                <form className="mt-6 mb-6" onSubmit={sendPayload}>
-                    <div className="grid grid-cols-2 grid-rows-4 gap-x-4 gap-y-3 w-max mx-auto">
-                        <label>
-                            <p className="mb-1">
-                                Product Name:
-                            </p>
-                            <input
-                                type="text" name="name"
-                                placeholder="No product"
-                                className={inputStyles}
-                                onInput={(e: React.ChangeEvent<HTMLInputElement>) => formDataUpdate("product", e.target.value)}
-                            />
-                        </label>
-                        <label>
-                            <p className="mb-1">
-                                Product Price:
-                            </p>
-                            <input
-                                type="number" name="price"
-                                placeholder="$ - - -" className={inputStyles}
-                                onInput={(e: React.ChangeEvent<HTMLInputElement>) => setOrderPrice(parseInt(e.target.value))}
-                            />
-                        </label>
-                        <label>
-                            <p className="mb-1">
-                                Create Date:
-                            </p>
-                            <input
-                                type="date" name="date" className={inputStyles}
-                                onInput={(e: React.ChangeEvent<HTMLInputElement>) => formDataUpdate("date", e.target.value)}
-                            />
-                        </label>
-                        <label>
-                            <p className="mb-1">
-                                Delivery Date:
-                            </p>
-                            <input
-                                type="date" name="delivery" className={inputStyles}
-                                onInput={(e: React.ChangeEvent<HTMLInputElement>) => formDataUpdate("delivery_date", e.target.value)}
-                            />
-                        </label>
-                        <label>
-                            <p className="mb-1">
-                                Product Code:
-                            </p>
-                            <input
-                                type="text" name="code" placeholder="TC-XXXX" className={inputStyles}
-                                onInput={(e: React.ChangeEvent<HTMLInputElement>) => formDataUpdate("code", e.target.value)}
-                            />
-                        </label>
-                        <label>
-                            <p className="mb-1">
-                                Phone Number:
-                            </p>
-                            <input
-                                type="tel" name="phone"
-                                placeholder="+57 XXX XXX XXXX" className={inputStyles}
-                                onInput={(e: React.ChangeEvent<HTMLInputElement>) => formDataUpdate("cel_number", e.target.value)}
-                            />
-                        </label>
-                        <label>
-                            <p className="mb-1">
-                                Delivery Status:
-                            </p>
-                            <input
-                                type="text" name="status"
-                                placeholder="Current state" className={inputStyles}
-                                onInput={(e: React.ChangeEvent<HTMLInputElement>) => setOrderStatus(parseInt(e.target.value))}
-                            />
-                        </label>
-                        <label>
-                            <p className="mb-1">
-                                Wallet User:
-                            </p>
-                            <input type="text" name="status" placeholder="Number wallet" className={inputStyles} />
-                        </label>
-                    </div>
-
-                    <button className="block my-5 mx-auto hover:border-black hover:border border-black border" type="submit">Submit</button>
-
-                    <hr className="shadow-[0px_0px_1px_0px_#cdcdcd]" />
-                </form>
-
-                            <hr className="shadow-[0px_0px_1px_0px_#cdcdcd]" />
-                        </form>        
+                        <hr className="shadow-[0px_0px_1px_0px_#cdcdcd]" />
+                    </form>
                     ) : context.navDashBtn2 ? (
                         <div className="flex flex-col gap-5 items-center pt-7">
                             <h2 className="text-[36px]">Preview Location</h2>
