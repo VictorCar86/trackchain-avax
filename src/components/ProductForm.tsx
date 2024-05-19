6c7bfba7aa08bed2c774712dbce1f52e9fe0c0b2
Joseth Calderon <stevencorredor04@gmail.com>
2024-05-19 14:32:14 -0500

    Finishing user and provider component completed

--- src/components/ProductForm.tsx
+++ src/components/ProductForm.tsx
@@ -1,87 +1,107 @@
-import QRcode from "../assets/images/QRcode";
+/* Components */
+import { useContext } from "react";
+import { TrackContext } from "../context/context";
 
 function ProductForm() {
     const inputStyles = "w-72 py-2 px-3 border border-gray-500 rounded-[12px] font-medium";
+    const context = useContext(TrackContext);
+
     return (
         <>
             <div className="p-5 font-bold">
-                <header className="flex justify-between px-14 mt-3.5 mb-8">
+                <header className="flex w-full gap-10 justify-between px-8 mt-3.5 mb-8">
                     <h1 className="text-[40px]">
                         Send your product
                     </h1>
                     <div className="flex gap-3">
-                        <button className="hover:border-black hover:border border-black border" type="button">
+                        <button 
+                        onClick={() => {
+                            context?.setNavDashBtn1(true);
+                            context?.setNavDashBtn2(false);
+                        }}
+                        className="hover:border-black hover:border border-black border" type="button">
                             ℹ️ Product Info
                         </button>
-                        <button className="hover:border-black hover:border border-black border" type="button">
+                        <button 
+                        onClick={() => {
+                            context?.setNavDashBtn2(true);
+                            context?.setNavDashBtn1(false);
+                        }}
+                        className="hover:border-black hover:border border-black border" type="button">
                             🗺️ Preview Location
                         </button>
                     </div>
                 </header>
 
                 <hr className="shadow-[0px_0px_1px_0px_#cdcdcd]" />
+                
+                {
+                    context.navDashBtn1 ? (
+                        <form className="mt-6 mb-6">
+                            <div className="grid grid-cols-2 grid-rows-4 gap-x-4 gap-y-3 w-max mx-auto">
+                                <label>
+                                    <p className="mb-1">
+                                        Product Name:
+                                    </p>
+                                    <input type="text" name="name" placeholder="Mesa PC" className={inputStyles} />
+                                </label>
+                                <label>
+                                    <p className="mb-1">
+                                        Product Price:
+                                    </p>
+                                    <input type="number" name="price" placeholder="$100" className={inputStyles} />
+                                </label>
+                                <label>
+                                    <p className="mb-1">
+                                        Create Date:
+                                    </p>
+                                    <input type="date" name="date" className={inputStyles} />
+                                </label>
+                                <label>
+                                    <p className="mb-1">
+                                        Delivery Date:
+                                    </p>
+                                    <input type="date" name="delivery" className={inputStyles} />
+                                </label>
+                                <label>
+                                    <p className="mb-1">
+                                        Product Code:
+                                    </p>
+                                    <input type="text" name="code" placeholder="123ed" className={inputStyles} />
+                                </label>
+                                <label>
+                                    <p className="mb-1">
+                                        Phone Number:
+                                    </p>
+                                    <input type="tel" name="phone" placeholder="+57 321 123 456" className={inputStyles} />
+                                </label>
+                                <label>
+                                    <p className="mb-1">
+                                        Delivery Status:
+                                    </p>
+                                    <input type="text" name="1" placeholder="Current state" className={inputStyles} />
+                                </label>
+                                <label>
+                                    <p className="mb-1">
+                                        Wallet User:
+                                    </p>
+                                    <input type="text" name="status" placeholder="0x4358c4455175b370E04b35D2c132993980294C62" className={inputStyles} />
+                                </label>
+                            </div>
 
-                <form className="mt-6 mb-6">
-                    <div className="grid grid-cols-2 grid-rows-4 gap-x-4 gap-y-3 w-max mx-auto">
-                        <label>
-                            <p className="mb-1">
-                                Product Name:
-                            </p>
-                            <input type="text" name="name" placeholder="No product" className={inputStyles} />
-                        </label>
-                        <label>
-                            <p className="mb-1">
-                                Product Price:
-                            </p>
-                            <input type="number" name="price" placeholder="$ - - -" className={inputStyles} />
-                        </label>
-                        <label>
-                            <p className="mb-1">
-                                Create Date:
-                            </p>
-                            <input type="date" name="date" className={inputStyles} />
-                        </label>
-                        <label>
-                            <p className="mb-1">
-                                Delivery Date:
-                            </p>
-                            <input type="date" name="delivery" className={inputStyles} />
-                        </label>
-                        <label>
-                            <p className="mb-1">
-                                Product Code:
-                            </p>
-                            <input type="text" name="code" placeholder="TC-XXXX" className={inputStyles} />
-                        </label>
-                        <label>
-                            <p className="mb-1">
-                                Phone Number:
-                            </p>
-                            <input type="tel" name="phone" placeholder="+57 XXX XXX XXXX" className={inputStyles} />
-                        </label>
-                        <label>
-                            <p className="mb-1">
-                                Delivery Status:
-                            </p>
-                            <input type="text" name="status" placeholder="Current state" className={inputStyles} />
-                        </label>
-                        <label>
-                            <p className="mb-1">
-                                Wallet User:
-                            </p>
-                            <input type="text" name="status" placeholder="Number wallet" className={inputStyles} />
-                        </label>
-                    </div>
+                            <button className="block my-5 mx-auto hover:border-black hover:border border-black border" type="button">Submit</button>
 
-                    <button className="block my-5 mx-auto hover:border-black hover:border border-black border" type="button">Submit</button>
-
-                    <hr className="shadow-[0px_0px_1px_0px_#cdcdcd]" />
-                </form>
-
-                {/* <div className="w-min mt-6 mx-auto rounded-2xl bg-gray-200">
-                    <QRcode className="h-44 w-44"/>
-                </div>
-                <p className="mt-2 mb-0 text-center">https://URL</p> */}
+                            <hr className="shadow-[0px_0px_1px_0px_#cdcdcd]" />
+                        </form>        
+                    ) : context.navDashBtn2 ? (
+                        <div className="flex flex-col gap-5 items-center pt-7">
+                            <h2 className="text-[36px]">Preview Location</h2>
+                            <div>
+                                <img src="" alt="" />
+                            </div>
+                        </div>
+                    ) : null
+                }
             </div>
         </>
     )
